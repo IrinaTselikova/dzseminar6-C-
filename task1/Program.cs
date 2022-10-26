6dd2e9aedb98d0b328a5a9dd7b1162cfb6b72d1d
unknown <hurina.irina@mail.ru>
2022-10-26 22:09:26 +0300

    task 1

--- task1/Program.cs
+++ task1/Program.cs
@@ -3,3 +3,40 @@
 1, -7, 567, 89, 223-> 3 */
 
 
+Console.WriteLine("Введите числа через запятую:");
+string str = Console.ReadLine();
+string[] strArray = str.Split(',');
+
+int [] numArray = new int[strArray.Length];
+
+for (int i = 0; i < strArray.Length; i++)
+{
+numArray[i] = int.Parse(strArray[i]);
+}
+
+Console.WriteLine("Вывод полученного массива:");
+
+
+for (int i = 0; i < numArray.Length; i++)
+{
+if(i < numArray.Length - 1)
+Console.Write($"{numArray[i]}, ");
+else
+Console.Write($"{numArray[i]}");
+}
+
+int NumberOfDigitsGreaterThanZero(int[] numArray)
+{
+    int count = 0;
+
+    for (int i = 0; i < numArray.Length; i++)
+    {
+        if (numArray[i]>0)
+        count += 1;
+    }
+    return count;
+}
+
+int count = NumberOfDigitsGreaterThanZero(numArray);
+Console.WriteLine();
+Console.WriteLine($"Вы ввели {count} чисел больше 0");
